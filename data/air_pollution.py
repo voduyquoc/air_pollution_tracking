468fc2c5dd2526f12e5bc1c4e054cca189882e3e
voduyquoc <voduyquockt@gmail.com>
2024-03-26 23:19:31 +0100

    Update

--- data/air_pollution.py
+++ data/air_pollution.py
@@ -1,11 +1,18 @@
 import json
 import requests
+import os
+import time 
+import requests
+import pandas as pd
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
-def get_data():
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bfdd4fa139528dd83f09a31cebb41760'
+def get_data(api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={api_key}"
     response = requests.get(url).json()
     print(json.dumps(response, indent=3))
 
 if __name__ == '__main__':
-    get_data()
+    get_data(API_KEY)
