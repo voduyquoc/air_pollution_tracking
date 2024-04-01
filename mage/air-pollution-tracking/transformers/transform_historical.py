65b577e107b91dee1645a5173daf04df311a685f
voduyquoc <voduyquockt@gmail.com>
2024-04-01 10:20:58 +0200

    Update

--- mage/air-pollution-tracking/transformers/transform_historical.py
+++ mage/air-pollution-tracking/transformers/transform_historical.py
@@ -21,8 +21,21 @@ def transform(data, *args, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your transformation logic here
-    data.rename(columns={'lon': 'longitude', 'lat': 'latitude', 'time': 'recorded_time'}, inplace=True)
+    new_column_names = {'lon': 'longitude', 
+                        'lat': 'latitude',
+                        'co': 'Carbon_monoxide_co',
+                        'no': 'Nitrogen_monoxide_no',
+                        'no2': 'Nitrogen_dioxide_no2',
+                        'o3': 'Ozone_o3',
+                        'so2': 'Sulphur_dioxide_so2',
+                        'pm2_5': 'Fine_particles_matter_pm2_5',
+                        'pm10': 'Coarse_particulate_matter_pm10',
+                        'nh3': 'Ammonia_nh3',
+                        'time': 'recorded_time'}
+    data.rename(columns=new_column_names, inplace=True)
     data['recorded_time'] = pd.to_datetime(data['recorded_time'], unit='s')
+    data['recorded_time'] = data['recorded_time'].dt.tz_localize('UTC')
+    data['local_time'] = data['recorded_time'].dt.tz_convert('Europe/Berlin')
     print(data.info())
     return data
 
