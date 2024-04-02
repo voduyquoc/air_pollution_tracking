d43ad2781c467d503fddeea63ee8c7f80d71501b
voduyquoc <voduyquockt@gmail.com>
2024-04-02 15:46:34 +0200

    Update

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.23.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name                      = "germany-air-quality-instance"
+  machine_type              = "e2-standard-4"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      size  = 30
+    }
+  }
+
+  network_interface {
+    network = var.network
+    access_config {
+    }
+  }
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 # days
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "stg_dataset" {
+  dataset_id                 = var.stg_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
+
+resource "google_bigquery_dataset" "prod_dataset" {
+  dataset_id                 = var.prod_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,55 @@
+variable "credentials" {
+  description = "Path to your google_credentials.json"
+  default     = ".key/google_credentials.json"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+  default     = "germany-air-quality"
+}
+
+variable "region" {
+  description = "Your project region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Your project zone"
+  default     = "us-central1-a"
+}
+
+variable "location" {
+  description = "Your project location"
+  default     = "US"
+}
+
+variable "gcs_bucket_name" {
+  description = "The name of your bucket. This should be unique across GCP"
+  default     = "germany-air-pollution-data"
+}
+
+variable "gcs_storage_class" {
+  description = "Storage class type for your bucket"
+  default     = "STANDARD"
+}
+
+variable "stg_bq_dataset" {
+  description = "The name of your dbt development dataset"
+  default     = "dbt_air_pollution"
+}
+
+variable "prod_bq_dataset" {
+  description = "The name of your dbt production dataset"
+  default     = "prod"
+}
+
+variable "vm_image" {
+  description = "Image for you VM"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
+
+variable "network" {
+  description = "Network for your instance"
+  default     = "default"
+}
+
